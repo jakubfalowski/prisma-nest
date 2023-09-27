eef730c32af39136723b63bd618879e63a3eac4b
Jakub Fałowski <kubafa99@gmail.com>
2023-09-27 07:54:10 +0200

    fix: Resignation heroku

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.POST, '0.0.0.0');
+  await app.listen(4000);
 }
 bootstrap();
